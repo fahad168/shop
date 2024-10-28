212ec0595a3e8532cb52662b94bf4df5d37cb89a
vs <vs@vss-MacBook-Pro.local>
2024-10-28 16:46:53 +0500

    admin migrations

--- lib/generators/shop/admin_generator.rb
+++ lib/generators/shop/admin_generator.rb
@@ -9,28 +9,28 @@ module Shop
 
       # Copying migration templates
       def copy_migration
-        migration_template "migrations/create_shop.rb", "db/migrate/create_shops.rb"
-        migration_template "migrations/create_products.rb", "db/migrate/create_products.rb"
-        migration_template "migrations/create_variants.rb", "db/migrate/create_variants.rb"
-        migration_template "migrations/create_sizes.rb", "db/migrate/create_sizes.rb"
+        migration_template "admin/migrations/create_shop.rb.tt", "db/migrate/create_shops.rb"
+        migration_template "admin/migrations/create_products.rb.tt", "db/migrate/create_products.rb"
+        migration_template "admin/migrations/create_variants.rb.tt", "db/migrate/create_variants.rb"
+        migration_template "admin/migrations/create_sizes.rb.tt", "db/migrate/create_sizes.rb"
       end
 
       # Copying model templates
       def copy_models
-        template "models/shop.rb", "app/models/shop.rb"
-        template "models/product.rb", "app/models/product.rb"
-        template "models/variant.rb", "app/models/variant.rb"
-        template "models/size.rb", "app/models/size.rb"
+        template "admin/models/shop.rb.tt", "app/models/shop.rb"
+        template "admin/models/product.rb.tt", "app/models/product.rb"
+        template "admin/models/variant.rb.tt", "app/models/variant.rb"
+        template "admin/models/size.rb.tt", "app/models/size.rb"
       end
 
       # Copying controller templates
       def copy_controllers
-        template "controllers/shops_controller.rb", "app/controllers/shops/shops_controller.rb"
+        template "admin/controllers/shops_controller.rb.tt", "app/controllers/shops/shops_controller.rb"
       end
 
       # Copying view templates
       def copy_views
-        template "views/shops/index.html.erb.tt", "app/views/shops/index.html.erb"
+        template "admin/views/shops/index.html.erb.tt", "app/views/shops/index.html.erb"
       end
 
       # Adding routes
