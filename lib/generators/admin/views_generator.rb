20262efcb770cb74867e78c4ce9714576edcb9e9
vs <vs@vss-MacBook-Pro.local>
2024-10-29 19:17:47 +0500

    Views

--- lib/generators/admin/views_generator.rb
+++ lib/generators/admin/views_generator.rb
@@ -30,13 +30,17 @@ module Admin
 
       # Copying view templates
       def copy_views
-        # template "views/shop_admin.html.erb", "app/views/layouts/shop_admin.html.erb"
-        template "views/shops/index.html.erb", "app/views/admin/shops/index.html.erb"
-        template "views/shops/new.html.erb", "app/views/admin/shops/new.html.erb"
-        template "views/shared/field_loader.html.erb", "app/views/admin/shared/_field_loader.html.erb"
-        template "views/shared/submit_button.html.erb", "app/views/admin/shared/_submit_button.html.erb"
-        template "views/shared/success_svg.html.erb", "app/views/admin/shared/_success_svg.html.erb"
-        template "views/dashboard/index.html.erb", "app/views/admin/dashboard/index.html.erb"
+        template_path = "views/shops/index.html.erb"
+        content = File.read(template_path)
+        create_file "app/views/admin/shops/index.html.erb", <<~EOF
+          #{content}
+        EOF
+        # template "views/shops/index.html.erb", "app/views/admin/shops/index.html.erb"
+        # template "views/shops/new.html.erb", "app/views/admin/shops/new.html.erb"
+        # template "views/shared/field_loader.html.erb", "app/views/admin/shared/_field_loader.html.erb"
+        # template "views/shared/submit_button.html.erb", "app/views/admin/shared/_submit_button.html.erb"
+        # template "views/shared/success_svg.html.erb", "app/views/admin/shared/_success_svg.html.erb"
+        # template "views/dashboard/index.html.erb", "app/views/admin/dashboard/index.html.erb"
       end
     end
   end
