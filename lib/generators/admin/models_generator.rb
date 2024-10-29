8eeb93e08231f4afa7ead6852df3df8f2ed36111
vs <vs@vss-MacBook-Pro.local>
2024-10-29 18:23:29 +0500

    layout file created

--- lib/generators/admin/models_generator.rb
+++ lib/generators/admin/models_generator.rb
@@ -33,13 +33,30 @@ module Admin
 
       # Copying view templates
       def copy_views
-        template "views/shop_admin.html.erb", "app/views/layouts/shop_admin.html.erb"
+        # template "views/shop_admin.html.erb", "app/views/layouts/shop_admin.html.erb"
         template "views/shops/index.html.erb", "app/views/admin/shops/index.html.erb"
         template "views/shops/new.html.erb", "app/views/admin/shops/new.html.erb"
         template "views/shared/field_loader.html.erb", "app/views/admin/shared/_field_loader.html.erb"
         template "views/shared/submit_button.html.erb", "app/views/admin/shared/_submit_button.html.erb"
         template "views/shared/success_svg.html.erb", "app/views/admin/shared/_success_svg.html.erb"
         template "views/dashboard/index.html.erb", "app/views/admin/dashboard/index.html.erb"
+      end
+
+      def create_layout_file
+        create_file "app/views/layouts/shop_admin.html.erb", <<~EOF
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <title>Admin Shop</title>
+              <meta name="viewport" content="width=device-width,initial-scale=1">
+              <script src="https://cdn.tailwindcss.com"></script>
+              <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+            </head>
+            <body>
+              <%= yield %>
+            </body>
+          </html>
+        EOF
       end
 
       # Adding routes
